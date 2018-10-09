9ffa2cba417b17e088f01dd44e3aef3721ed36ae
nitin panuganti <nitin.noornook@gmail.com>
2018-10-09 11:26:45 +0530

    Basic Setup

--- scripts/xnix/cen-bot.sh
+++ scripts/xnix/cen-bot.sh
@@ -1,13 +1,12 @@
 #! /bin/sh
 
 cd $(dirname $0)
-
+pwd
 #clear
-
+export PYTHONPATH=./program-y/src:
 if [[ -z "${PYTHONPATH}" ]]; then
   echo "PYTHONPATH is undefined"
   exit
 fi
 
 python3 -m programy.clients.events.console.client --config ../../config/config.mysql.yaml --cformat yaml --logging ../../config/logging.yaml
-
