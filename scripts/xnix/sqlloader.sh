5b1ad79df7dc97d21143437093d91e441aee47e6
nitin panuganti <nitin.noornook@gmail.com>
2018-10-10 23:15:40 +0530

    AIML in place

--- scripts/xnix/sqlloader.sh
+++ scripts/xnix/sqlloader.sh
@@ -10,7 +10,7 @@ export PYTHONPATH=../../program-y/src
 
 # Categories
 python3 -m programy.storage.stores.sql.loader -c --entity categories --url mysql+pymysql://$DBAUTH/programy --dir ../../storage/aiml --subdir --extension=.aiml --verbose
-#
+
 #
 # for i in ../../storage/dirs/*
 # do
@@ -25,16 +25,16 @@ python3 -m programy.storage.stores.sql.loader -c --entity categories --url mysql
 # done
 #
 #
-# for i in ../../storage/conf/*.conf
-# do
-# 	echo $i
-# 	python3 -m programy.storage.stores.sql.loader  -c --entity $(basename $i .conf) --url mysql+pymysql://$DBAUTH/programy --file $i --verbose
-# done
+for i in ../../storage/conf/*.conf
+do
+	echo $i
+	python3 -m programy.storage.stores.sql.loader  -c --entity $(basename $i .conf) --url mysql+pymysql://$DBAUTH/programy --file $i --verbose
+done
 #
 #
 #
 # # License Keys
 # python3 -m programy.storage.stores.sql.loader -c --entity licenses --url mysql+pymysql://$DBAUTH/programy --file ../../storage/conf/license.keys --verbose
-#
-# # UserGroups
-## python3 -m programy.storage.stores.sql.loader -c --entity usergroups --url mysql+pymysql://$DBAUTH/programy --file ../../storage/conf/usergroups.yaml --verbose
+
+# UserGroups
+# python3 -m programy.storage.stores.sql.loader -c --entity usergroups --url mysql+pymysql://$DBAUTH/programy --file ../../storage/conf/usergroups.yaml --verbose
