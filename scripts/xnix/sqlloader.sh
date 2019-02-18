6fe575b63096d993c775c9d4716cb6e1310f781d
nitin panuganti <nitin.noornook@gmail.com>
2019-02-18 21:57:26 +0530

    delivery boys done

--- scripts/xnix/sqlloader.sh
+++ scripts/xnix/sqlloader.sh
@@ -6,35 +6,35 @@ cd $(dirname $0)
 
 DBAUTH=root@localhost
 
-export PYTHONPATH=../../../programy/src
+export PYTHONPATH=../../programy/src
 
 # Categories
-python3 -m programy.storage.stores.sql.loader -c --entity categories --url mysql+pymysql://$DBAUTH/programy --dir ../../storage/aiml/cen --subdir --extension=.aiml --verbose
+python3 -m programy.storage.stores.sql.loader -c --entity categories --url mysql+pymysql://$DBAUTH/programy --dir ../../storage/aiml --subdir --extension=.aiml --verbose
 
-#
-# for i in ../../storage/dirs/*
-# do
-# 	echo $i
-# 	python3 -m programy.storage.stores.sql.loader  -c --entity $(basename $i ) --url mysql+pymysql://$DBAUTH/programy --dir $i  --extension=.txt --verbose
-# done
-#
-# for i in ../../storage/files/*.txt
-# do
-# 	echo $i
-# 	python3 -m programy.storage.stores.sql.loader  -c --entity $(basename $i .txt) --url mysql+pymysql://$DBAUTH/programy --file $i --verbose
-# done
-#
-#
-# for i in ../../storage/conf/*.conf
-# do
-# 	echo $i
-# 	python3 -m programy.storage.stores.sql.loader  -c --entity $(basename $i .conf) --url mysql+pymysql://$DBAUTH/programy --file $i --verbose
-# done
-#
-#
-#
-# # License Keys
-# python3 -m programy.storage.stores.sql.loader -c --entity licenses --url mysql+pymysql://$DBAUTH/programy --file ../../storage/conf/license.keys --verbose
-#
-# # UserGroups
-# # python3 -m programy.storage.stores.sql.loader -c --entity usergroups --url mysql+pymysql://$DBAUTH/programy --file ../../storage/conf/usergroups.yaml --verbose
+
+for i in ../../storage/dirs/*
+do
+	echo $i
+	python3 -m programy.storage.stores.sql.loader  -c --entity $(basename $i ) --url mysql+pymysql://$DBAUTH/programy --dir $i  --extension=.txt --verbose
+done
+
+for i in ../../storage/files/*.txt
+do
+	echo $i
+	python3 -m programy.storage.stores.sql.loader  -c --entity $(basename $i .txt) --url mysql+pymysql://$DBAUTH/programy --file $i --verbose
+done
+
+
+for i in ../../storage/conf/*.conf
+do
+	echo $i
+	python3 -m programy.storage.stores.sql.loader  -c --entity $(basename $i .conf) --url mysql+pymysql://$DBAUTH/programy --file $i --verbose
+done
+
+
+
+# License Keys
+python3 -m programy.storage.stores.sql.loader -c --entity licenses --url mysql+pymysql://$DBAUTH/programy --file ../../storage/conf/license.keys --verbose
+
+# UserGroups
+# python3 -m programy.storage.stores.sql.loader -c --entity usergroups --url mysql+pymysql://$DBAUTH/programy --file ../../storage/conf/usergroups.yaml --verbose
